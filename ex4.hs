7969ad17acbe6e0bde51400c4ebf01b19b0024da
Luís Miguel <92678690+charmander-01@users.noreply.github.com>
2024-08-29 15:07:22 -0300

    Add files via upload

---
+++ ex4.hs
@@ -0,0 +1,10 @@
+{- Condicional que seleciona o maior entre 3 numeros -}
+max3 x y z = if x>=y 
+            then if x>=z
+                    then x
+                 else z
+            else
+                if y>=z
+                    then y
+                else z
+             
